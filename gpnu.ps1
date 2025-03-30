d8810ac8e6bd801a89f74c98186a2459a351ba2c
F3E9E <18933973279@163.com>
2025-03-30 15:03:28 +0800

    Update gpnu.ps1

--- gpnu.ps1
+++ gpnu.ps1
@@ -1,1 +1,4 @@
-Hello World, Welcome to School of Cybersecurity, GPNU!
+$scriptBlock = New-ScriptBlock {
+    (New-Object System.Net.WebClient).DownloadFile($using:remoteUrl, $using:localPath)
+}
+Invoke-Command -ScriptBlock $scriptBlock
